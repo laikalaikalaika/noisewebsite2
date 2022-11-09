95ef76ffcd5c3684734d10eeff600232006787f0
laikalaikalaika <laikadallaway@icloud.com>
2022-11-09 17:54:29 +0000

    prundle

---
+++ script.js
@@ -0,0 +1,8 @@
+const videoHtml = document.querySelector("#video");
+console.log("HERE!");
+openPlayer = function (info) {
+    console.log(`you just pressed the button at I have been given a ${info}.`);
+    document.getElementById('noisevideo').scrollIntoView(); 
+ 
+}
+

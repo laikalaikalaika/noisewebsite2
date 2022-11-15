51f368a2d7c6137c732fc5aed83034bee2a9f4fb
laikalaikalaika <laikadallaway@icloud.com>
2022-11-15 20:07:59 +0000

    broken ass clock

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ openPlayer = function (info) {
     document.getElementById('noisevideo').scrollIntoView(); 
     document.getElementById('noisevideo').play(); 
 }
+function displayDate() {
+    console.log('New Date', new Date());
+    var today = new Date()
+    document.getElementById("showDate").innerHTML = (today.toLocaleString());
+    console.log(today.toLocaleString());
+
+    
+  }
+  displayDate()
+
+  // I STILL cannot make this work TT

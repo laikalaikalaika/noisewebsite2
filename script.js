0993455e87a58b78e7b7068927a9c818f9837265
laikalaikalaika <laikadallaway@icloud.com>
2022-11-15 19:14:11 +0000

    updates

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ console.log("HERE!");
 openPlayer = function (info) {
     console.log(`you just pressed the button at I have been given a ${info}.`);
     document.getElementById('noisevideo').scrollIntoView(); 
- 
+    document.getElementById('noisevideo').play(); 
 }
-
